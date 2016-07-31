3dff23f57a843fcbf00e0f3726fba1a9b33b29ee
Johnry Mangonon <johnry.mangonon@bbc.co.uk>
2016-07-31 15:37:23 +0100

    re-add first challenge

---
+++ GeoQuiz Challenge 1/app/src/test/java/com/skeeno/android/geoquiz/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.skeeno.android.geoquiz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void testCheckAnswer() throws Exception {
+        QuizActivity quizActivity = new QuizActivity();
+    }
+}
